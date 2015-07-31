583b7f7e11a9fdb5d910884983c1d1694b5fe047
Andreas Würl <andi@tryb.de>
2015-07-31 22:45:49 +0200

    added fibonacci stream generator

--- src/test/java/com/mercateo/demo/functionaljava/StreamTest.java
+++ src/test/java/com/mercateo/demo/functionaljava/StreamTest.java
@@ -1,5 +1,9 @@
 package com.mercateo.demo.functionaljava;
 
+import fj.F;
+import fj.P;
+import fj.P2;
+import fj.data.Array;
 import fj.data.Stream;
 import org.junit.Test;
 
@@ -13,5 +17,19 @@ public class StreamTest {
         assertThat(stream.drop(5).take(3)).containsExactly(5, 6, 7);
     }
 
+    @Test
+    public void testFibonacciIteration() {
+        final P2<Integer, Integer> seed = P.p(1, 1);
+
+        final F<P2<Integer, Integer>, P2<Integer, Integer>> step = p -> P.p(p._2(), p._1() + p._2());
+
+        final Array<Integer> result = Stream
+                .iterate(step, seed)
+                .take(10)
+                .map(P2::_1)
+                .toArray();
+
+        assertThat(result).containsExactly(1, 1, 2, 3, 5, 8, 13, 21, 34, 55);
+    }
 
 }
