d963883367bdb5474ca31dc63913f9a277ead404
Andreas Würl <andi@tryb.de>
2015-11-25 00:30:49 +0100

    added some more javaslang tests

---
+++ src/test/java/com/mercateo/demo/functionaljava/FunctionTest.java
@@ -0,0 +1,43 @@
+package com.mercateo.demo.functionaljava;
+
+import javaslang.Function1;
+import javaslang.control.Option;
+import org.junit.Test;
+
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FunctionTest {
+
+    @Test
+    public void testFunction() {
+        String test = "Hello World!";
+
+        int hashCode = test.hashCode();
+
+        final Function1<Object, Integer> hash = Object::hashCode;
+        assertThat(hash.apply(test)).isEqualTo(hashCode);
+
+        final Function<Object, Integer> hashCache = Function1.of(Object::hashCode).memoized();
+ dd       assertThat(hashCache.apply(test)).isEqualTo(hashCode);
+    }
+
+    @Test
+    public void testLift() {
+        final Function1<Float, Float> partial = Function1.of(this::partial);
+
+        final Function1<Float, Option<Float>> total = Function1.lift(partial);
+
+        assertThat(total.apply(2.0f)).contains(0.5f);
+        assertThat(total.apply(0f)).isEmpty();
+    }
+
+    private Float partial(Float x) {
+        if (x > 0) {
+            return 1 / x;
+        } else {
+            throw new IllegalStateException("invalid input");
+        }
+    }
+}
---
+++ src/test/java/com/mercateo/demo/functionaljava/TuplesTest.java
@@ -0,0 +1,22 @@
+package com.mercateo.demo.functionaljava;
+
+import javaslang.Tuple;
+import javaslang.Tuple4;
+import lombok.val;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class TuplesTest {
+
+    @Test
+    public void testTuple() {
+        final Tuple4<String, Double, Long, Boolean> tuple = Tuple.of("Name", 2.3, 1L, true);
+
+        assertThat(tuple._1).isEqualTo("Name");
+        assertThat(tuple._2).isEqualTo(2.3);
+        assertThat(tuple._3).isEqualTo(1L);
+        assertThat(tuple._4).isTrue();
+    }
+}
